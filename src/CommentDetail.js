e0e8794b66683bbe6f626f1507f9d1dfdfc04527
Matthew Wright <matthewbwright@gmail.com>
2018-12-21 10:39:14 -0700

    refactors code to pass props for avatar image

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -6,7 +6,7 @@ const CommentDetail = props => {
   return (
     <div className="comment">
       <a href="/" className="avatar">
-        <img src={faker.image.avatar()} alt="avatar" />
+        <img src={props.avatar} alt="avatar" />
       </a>
       <div className="content">
         <a href="/" className="author">
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
 
 import CommentDetail from './CommentDetail';
 
@@ -10,16 +11,19 @@ const App = () => {
         author="Sam"
         timeAgo="Today at 4:45 PM"
         commentText="I love this!"
+        avatar={faker.image.avatar()}
       />
       <CommentDetail
         author="Alex"
         timeAgo="Today at 2:45 AM"
         commentText="When did you do this?"
+        avatar={faker.image.avatar()}
       />
       <CommentDetail
         author="Jane"
         timeAgo="Yesterday at 5:55 PM"
         commentText="We need to chat."
+        avatar={faker.image.avatar()}
       />
     </div>
   );
