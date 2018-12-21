8946ebeeb0f9668f430e80774ff5880dcb6c4b33
Matthew Wright <matthewbwright@gmail.com>
2018-12-21 10:00:11 -0700

    adds avatar image with faker

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const App = () => {
     <div className="ui container comments">
       <div className="comment">
         <a href="/" className="avatar">
-          <img src="" alt="avatar" />
+          <img src={faker.image.avatar()} alt="avatar" />
         </a>
         <div className="content">
           <a href="/" className="author">
